86228fba9fcc4a20997fd4169a60e7680043659f
GuilhermeMelo01 <Meloguilherme1994@gmail.com>
2022-06-10 16:57:04 -0300

    2 exercicio (SomaImparesConsecutivos3, SomaParesConsecutivos)

--- src/Divisores1.java
+++ src/Divisores1.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 
+//        Ler um número inteiro N e calcular todos os seus divisores.
+//
+//        ENTRADA
+//        O arquivo de entrada contém um valor inteiro.
+//
+//        SAÍDA
+//        Escreva todos os divisores positivos de N, um valor por linha.
+
 public class Divisores1 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -8,10 +16,10 @@ public class Divisores1 {
 
         n = in.nextInt();
 
-        for (int i = n; i >= 0; i--) {
+        for (int i = 0; i <= n; i++) {
             if (i != 0){
                 if (n % i == 0){
-                    System.out.println(i); //Codigo não terminado
+                    System.out.println(i);
                 }
             }
         }
---
+++ src/SomaParesConsecutivos.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class SomaParesConsecutivos {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int x, soma;
+
+        while ((x = in.nextInt()) != 0) {
+            soma = 0;
+            if (x % 2 == 0) {
+                for (int i = 0; i < 5; i++) {
+                    soma += x;
+                    x += 2;
+                }
+                System.out.println(soma);
+            }else {
+                x+=1;
+                for (int i = 0; i < 5; i++) {
+                    soma += x;
+                    x += 2;
+                }
+                System.out.println(soma);
+            }
+        }
+    }
+}
