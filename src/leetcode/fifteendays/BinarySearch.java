13b85436d482c9c32677099f93095a459edf5233
Guilhermemelo01 <meloguilherme1994@gmail.com>
2022-11-22 14:25:50 -0300

    LeetCode desafio 15 dias - BinarySearch e VersionControl

---
+++ src/leetcode/fifteendays/BinarySearch.java
@@ -0,0 +1,20 @@
+package leetcode.fifteendays;
+
+public class BinarySearch {
+    public int search(int[] nums, int target) {
+        int inicio = 0;
+        int fim = nums.length-1;
+        int meio;
+        while(inicio <= fim){
+            meio = (inicio + fim) / 2;
+            if(nums[meio] == target){
+                return meio;
+            }else if(nums[meio] < target){
+                inicio = meio+1;
+            }else{
+                fim = meio-1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ src/leetcode/fifteendays/VersionControl.java
@@ -0,0 +1,22 @@
+package leetcode.fifteendays;
+
+public class VersionControl {
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+//    public class Solution extends VersionControl {
+//        public int firstBadVersion(int n) {
+//            int inicio = 1, fim = n;
+//
+//            while(inicio < fim){
+//                int meio = inicio + (fim - inicio) / 2;
+//                if(isBadVersion(meio) == true){
+//                    fim = meio;
+//                }else{
+//                    inicio = meio+1;
+//                }
+//            }
+//            return inicio;
+//        }
+//    }
+}
